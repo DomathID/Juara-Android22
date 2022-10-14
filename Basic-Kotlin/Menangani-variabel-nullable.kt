59e94cf57cc3c2b3392ae84653500c30f34a7a9f
Bang Domath ID <bangdompret@gmail.com>
2022-10-14 23:36:14 +0700

    Create Menangani-variabel-nullable.kt

---
+++ Basic-Kotlin/Menangani-variabel-nullable.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val favoriteActor: String? = "Sandra Oh"
+
+    val lengthOfName = favoriteActor?.length ?: 0
+
+    println("The number of characters in your favorite actor's name is $lengthOfName.")
+}
