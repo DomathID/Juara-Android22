99bedbb70d74f7b53a535ceabc543160931750ef
Bang Domath ID <bangdompret@gmail.com>
2022-10-14 23:39:34 +0700

    Create Notifikasi-seluler.kt

---
+++ Basic-Kotlin/notif/Notifikasi-seluler.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    // Fill in the code.
+}
